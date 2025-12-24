742822cdec6a5fd8a4fe728f794649f46b8c09a6
OctaInsight <octainsight@gmail.com>
2025-12-24 11:58:55 +0100

    3_Data_Exploration_and_Visualization.py

--- pages/3_Data_Exploration_and_Visualization.py
+++ pages/3_Data_Exploration_and_Visualization.py
@@ -15,6 +15,7 @@ sys.path.insert(0, str(project_root))
 
 from core.config import get_config, initialize_session_state
 from core.utils import display_error, display_warning, display_info, get_color_for_index
+from core.shared_sidebar import render_app_sidebar  
 from core.viz.plot_time import plot_single_variable
 
 # Initialize
