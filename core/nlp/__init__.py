25966ff885d3f8220260be1da621ce9cbb7e92d1
OctaInsight <octainsight@gmail.com>
2025-12-25 14:03:06 +0100

    __init__.py

--- core/nlp/__init__.py
+++ core/nlp/__init__.py
@@ -1,1 +1,29 @@
+"""
+NLP module for scenario analysis
+"""
 
+from .lang_detect import detect_language, validate_english
+from .scenario_segment import segment_scenarios
+from .parameter_extract import extract_parameters_from_scenarios
+from .value_parse import parse_value_string, normalize_unit, format_value
+from .mapping import suggest_variable_mapping, create_mapping, apply_mapping
+from .clean_text import generate_cleaned_scenario_text, export_to_json, generate_comparison_table
+from .schema import Scenario, ScenarioItem
+
+__all__ = [
+    'detect_language',
+    'validate_english',
+    'segment_scenarios',
+    'extract_parameters_from_scenarios',
+    'parse_value_string',
+    'normalize_unit',
+    'format_value',
+    'suggest_variable_mapping',
+    'create_mapping',
+    'apply_mapping',
+    'generate_cleaned_scenario_text',
+    'export_to_json',
+    'generate_comparison_table',
+    'Scenario',
+    'ScenarioItem'
+]
