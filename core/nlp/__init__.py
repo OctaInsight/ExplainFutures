18b9d00eef0456579e3dbe018ade6bdb768e0f62
OctaInsight <octainsight@gmail.com>
2025-12-25 16:16:33 +0100

    __init__.py

--- core/nlp/__init__.py
+++ core/nlp/__init__.py
@@ -9,6 +9,15 @@ from .value_parse import parse_value_string, normalize_unit, format_value
 from .mapping import suggest_variable_mapping, create_mapping, apply_mapping
 from .clean_text import generate_cleaned_scenario_text, export_to_json, generate_comparison_table
 from .schema import Scenario, ScenarioItem
+from .unification import (
+    unify_parameter, 
+    unify_extracted_items, 
+    deduplicate_items,
+    merge_similar_parameters,
+    add_user_synonym,
+    get_all_canonical_parameters
+)
+from .templates import extract_with_templates, get_template_count
 
 __all__ = [
     'detect_language',
@@ -25,5 +34,13 @@ __all__ = [
     'export_to_json',
     'generate_comparison_table',
     'Scenario',
-    'ScenarioItem'
+    'ScenarioItem',
+    'unify_parameter',
+    'unify_extracted_items',
+    'deduplicate_items',
+    'merge_similar_parameters',
+    'add_user_synonym',
+    'get_all_canonical_parameters',
+    'extract_with_templates',
+    'get_template_count'
 ]
