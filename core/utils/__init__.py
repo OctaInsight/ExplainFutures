d878a7b4eab5ddd0f87f0fc92c2e093d064ac4d2
OctaInsight <octainsight@gmail.com>
2025-12-29 13:26:47 +0100

    Update __init__.py

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -1,1 +1,34 @@
+"""
+Utility functions for ExplainFutures
+"""
 
+from .helpers import (
+    display_error,
+    display_success,
+    display_warning,
+    display_info,
+    detect_datetime_columns,
+    detect_numeric_columns,
+    format_percentage,
+    format_number
+)
+
+from .project_loader import (
+    load_complete_project_data,
+    load_project_on_open,
+    ensure_project_data_loaded
+)
+
+__all__ = [
+    'display_error',
+    'display_success',
+    'display_warning',
+    'display_info',
+    'detect_datetime_columns',
+    'detect_numeric_columns',
+    'format_percentage',
+    'format_number',
+    'load_complete_project_data',
+    'load_project_on_open',
+    'ensure_project_data_loaded',
+]
