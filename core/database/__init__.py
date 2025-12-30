eee3b3b137dbed3537b0f30200ee3acae4a3412b
OctaInsight <octainsight@gmail.com>
2025-12-30 14:32:37 +0100

    Update __init__.py

--- core/database/__init__.py
+++ core/database/__init__.py
@@ -2,6 +2,7 @@
 ExplainFutures Database Package
 """
 
-from .supabase_manager import SupabaseManager, get_db_manager
+from .supabase_manager import get_db_manager, SupabaseManager
 
-__all__ = ['SupabaseManager', 'get_db_manager']
+__all__ = ["get_db_manager", "SupabaseManager"]
+
