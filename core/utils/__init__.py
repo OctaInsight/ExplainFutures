db3c86d63a8836e57268643435cec517ccff3acc
OctaInsight <octainsight@gmail.com>
2025-12-30 14:34:56 +0100

    Update __init__.py

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -1,7 +1,3 @@
-"""
-Utility functions for ExplainFutures
-"""
-
 from .helpers import (
     display_error,
     display_success,
@@ -11,26 +7,22 @@ from .helpers import (
     detect_numeric_columns,
     format_percentage,
     format_number,
-    get_color_for_index  # ADDED
 )
 
 from .project_loader import (
-    load_complete_project_data,
     load_project_on_open,
-    ensure_project_data_loaded
+    ensure_project_loaded,
 )
 
 __all__ = [
-    'display_error',
-    'display_success',
-    'display_warning',
-    'display_info',
-    'detect_datetime_columns',
-    'detect_numeric_columns',
-    'format_percentage',
-    'format_number',
-    'get_color_for_index',  # ADDED
-    'load_complete_project_data',
-    'load_project_on_open',
-    'ensure_project_data_loaded',
+    "display_error",
+    "display_success",
+    "display_warning",
+    "display_info",
+    "detect_datetime_columns",
+    "detect_numeric_columns",
+    "format_percentage",
+    "format_number",
+    "load_project_on_open",
+    "ensure_project_loaded",
 ]
